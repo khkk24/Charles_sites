59dd049f5f2481d31e3c1f851e08f720366d5a07
Georges Guy Gustinvil <georgeslyangus@gmail.com>
2024-09-27 14:55:25 -0300

    Init Service

--- site_charles/src/components/organisms/ServicesSection.js
+++ site_charles/src/components/organisms/ServicesSection.js
@@ -6,25 +6,21 @@ import './Styles.css';
 
 const data = [
     {
-        "id": 1,
         "title":"Serviço I",
         "text" : "Some quick example text to build on the card title and make up the bulk of the card's content.",
         "image": "/images/service/services_1.jpg"
     },
     {
-        "id": 2,
         "title":"Serviço II",
         "text" : "Some quick example text to build on the card title and make up the bulk of the card's content.",
         "image": "/images/service/services_2.jpg"
     },
     {
-        "id": 3,
         "title":"Serviço III",
         "text" : "Some quick example text to build on the card title and make up the bulk of the card's content.",
         "image": "/images/service/services_1.jpg"
     },
     {
-        "id": 4,
         "title":"Serviço IV",
         "text" : "Some quick example text to build on the card title and make up the bulk of the card's content.",
         "image": "/images/service/services_2.jpg"
@@ -36,8 +32,8 @@ const Service = () => {
         <div className='service-wrapper'>
           <h1 id='service-title'> Serviços </h1>
           <Row className='teste1' >
-            { data.map((item) => (
-                <Col className='teste' key= {item.id}> <Card data= { item } /> </Col>
+            { data.map((item, index) => (
+                <Col className='teste' key= {index}> <Card data= { item } /> </Col>
             ))}
             
           </Row>
